7a3e7dd2659cd21b1fb3d977fbd5394d9278cba9
Shivam Prajapati <shivamprajapati.git@gmail.com>
2020-11-05 21:48:58 +0530

    Sliding window counter

--- Apache-storm/src/main/java/DataStructures/SlotBasedCounter.java
+++ Apache-storm/src/main/java/DataStructures/SlotBasedCounter.java
@@ -7,28 +7,28 @@ import java.util.Map;
 import java.util.Set;
 
 public final class SlotBasedCounter<T> implements Serializable {
-    private Map<T,long[]> objToCounts = new HashMap<T, long[]>();
+    private Map<T, long[]> objToCounts = new HashMap<T, long[]>();
     private int numSlots;
 
-    public SlotBasedCounter(int numSlots){
-        if(numSlots<=0){
-            throw  new IllegalArgumentException("Number of Slots must be greater tha 0");
+    public SlotBasedCounter(int numSlots) {
+        if (numSlots <= 0) {
+            throw new IllegalArgumentException("Number of Slots must be greater tha 0");
         }
         this.numSlots = numSlots;
     }
 
-    public long incrementCount(T obj, int slot){
+    public long incrementCount(T obj, int slot) {
         long[] counts = objToCounts.get(obj);
-        if(counts == null){
+        if (counts == null) {
             return 0;
         }
         return counts[slot];
     }
 
-    public Map<T, Long> getCounts(){
+    public Map<T, Long> getCounts() {
         Map<T, Long> result = new HashMap<T, Long>();
-        for(T obj : objToCounts.keySet()){
-            result.put(obj,computeTotalCount(obj));
+        for (T obj : objToCounts.keySet()) {
+            result.put(obj, computeTotalCount(obj));
         }
         return result;
     }
@@ -36,23 +36,24 @@ public final class SlotBasedCounter<T> implements Serializable {
     private long computeTotalCount(T obj) {
         long[] curr = objToCounts.get(obj);
         long total = 0;
-        for(long l : curr){
-            total+=l;
+        for (long l : curr) {
+            total += l;
         }
         return total;
     }
 
-    public void wipeSlot(int slot){
-        for(T obj : objToCounts.keySet()){
+    public void wipeSlot(int slot) {
+        for (T obj : objToCounts.keySet()) {
             resetSlotCountToZero(obj, slot);
         }
     }
 
     private void resetSlotCountToZero(T obj, int slot) {
         long[] counts = objToCounts.get(obj);
-        counts[slot]=0;
+        counts[slot] = 0;
     }
-    private boolean shouldBeRemovedFromCounter(T obj){
+
+    private boolean shouldBeRemovedFromCounter(T obj) {
         return computeTotalCount(obj) == 0;
     }
 
