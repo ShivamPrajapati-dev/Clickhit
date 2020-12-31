add12da0fbcd48cbaa2a5017b376d7c2fd0cc610
Shivam Prajapati <shivamprajapati.git@gmail.com>
2021-01-01 01:43:47 +0530

    update

--- _search-service/src/use-case/add-data.js
+++ _search-service/src/use-case/add-data.js
@@ -1,15 +1,66 @@
 module.exports = function makeAddData({client}){
+    
     return async function adddata(info){
-        
-        const data = await client.index({
-            index:info.index,
-            id:info.id,                 // use mongodb id
-            body:info.body
-        });
+
+        if(info.index == "prefs"){
+
+            const {body} = await client.get({
+                index:"posts",
+                id:info.body.activityId
+            })
+
+            const count_data = await client.count({
+                index:"prefs",
+                body:{
+                    query:{
+                        term:{
+                            "username":index.body.userId
+                        }
+                    }
+                }
+            });
+
+            if(count_data.count == 0){
+
+                await client.index({
+                    index:info.index,
+                    id:info.body.userId,                 // use mongodb id
+                    body:{
+                        "username":info.body.userId,
+                        "likes":[]                  // create an empty array initially
+                    }
+                });
+
+            }else{
+                
+                await client.update({                // update likes array with the post the user liked
+                    index:info.index,
+                    id:info.body.userId,
+                    body:{
+                        script:{
+                            lang:"painless",
+                            source:"ctx._source.likes.add(params.like)",
+                            params:{
+                                like:body._source
+                            }
+                        }
+                    }
+                })
+            }
+
+        }else{
+
+            await client.index({
+                index:info.index,
+                id:info.id,                 // use mongodb id
+                body:info.body
+            });
+
+        }
 
         await client.indices.refresh({index:info.index});
 
-        return data;
+        return {success:true};
 
     }
 }
--- _search-service/src/use-case/search-as-you-type.js
+++ _search-service/src/use-case/search-as-you-type.js
@@ -5,12 +5,12 @@ module.exports = function buildMakeSAYT({client}){
         for(field of info.body.field){
             search_fields.push(field);              // type of data user want to search e.g username, tags etc
             search_fields.push(`${field}._2gram`);          // split into phrases of two words
-            search_fields.push(`${field}._2gram`);
-            search_fields.push(`${field}_index_prefix`);        // split each token into prefixes of tokens
+            search_fields.push(`${field}._3gram`);
+            search_fields.push(`${field}_index_prefix`);        // split each token into prefixes of tokens:- s, sh, shi, shiv...
         }
 
         const {body} = await client.search({
-            index:info.index,
+            index:info.index,                       // provide csv to search in multiple indices
             body:{
                 query:{
                     multi_match:{
